0c65d07441cef4dc2c91dff9221501b9ed552929
Lon Ingram <lingram@rgmadvisors.com>
2011-04-21 01:42:05 -0500

    First cut of code that reports displacement

--- inertial.coffee
+++ inertial.coffee
@@ -1,20 +1,47 @@
 x = 0
 y = 0
 r = 0
-v = 0
-t = Date.now()
-tLastMotionEvent = null
-body = $('body')
+v =
+    x: 0
+    y: 0
+    z: 0
 
+t = Date.now()
+h = null
+
+INCHES_PER_METER = 39.3700787
+
+tLastMotionEvent = null
+
+body = $('body')
 printTemplate = _.template '<span><%= s %></span>'
 print = (s) ->
     body.append printTemplate s: s
 
+report = -> print motionTemplate
+        x: x * INCHES_PER_METER
+        y: y * INCHES_PER_METER
+        z: z * INCHES_PER_METER
+
 motionTemplate = _.template 'agx: <%= x %>, agy: <%= y %>, agz: <%= z %>'
 onMotion = (e) ->
-    window.alert 'Got motion...'
-    print motionTemplate e.accelerationIncludingGravity
+    dt = Date.now() - t
+    t += dt
+    ti = e.interval
+    a = e.acceleration
 
-#$().bind('devicemotion', onMotion)
+    v.x += a.x * ti
+    v.y += a.y * ti
+    v.z += a.z * ti
+
+    # XXX: this is wrong. weight previous and current v by dt - ti and ti, respectively
+    x += v.x * dt
+    y += v.y * dt
+    z += v.z * dt
+
+    if h?
+        clearTimeout h
+    h = setTimeout report
+
 window.addEventListener 'devicemotion', onMotion, false
 window.alert 'Starting'
--- inertial.js
+++ inertial.js
@@ -1,10 +1,16 @@
 (function() {
-  var body, motionTemplate, onMotion, print, printTemplate, r, t, tLastMotionEvent, v, x, y;
+  var INCHES_PER_METER, body, h, motionTemplate, onMotion, print, printTemplate, r, report, t, tLastMotionEvent, v, x, y;
   x = 0;
   y = 0;
   r = 0;
-  v = 0;
+  v = {
+    x: 0,
+    y: 0,
+    z: 0
+  };
   t = Date.now();
+  h = null;
+  INCHES_PER_METER = 39.3700787;
   tLastMotionEvent = null;
   body = $('body');
   printTemplate = _.template('<span><%= s %></span>');
@@ -13,10 +19,30 @@
       s: s
     }));
   };
+  report = function() {
+    return print(motionTemplate({
+      x: x * INCHES_PER_METER,
+      y: y * INCHES_PER_METER,
+      z: z * INCHES_PER_METER
+    }));
+  };
   motionTemplate = _.template('agx: <%= x %>, agy: <%= y %>, agz: <%= z %>');
   onMotion = function(e) {
-    window.alert('Got motion...');
-    return print(motionTemplate(e.accelerationIncludingGravity));
+    var a, dt, ti;
+    dt = Date.now() - t;
+    t += dt;
+    ti = e.interval;
+    a = e.acceleration;
+    v.x += a.x * ti;
+    v.y += a.y * ti;
+    v.z += a.z * ti;
+    x += v.x * dt;
+    y += v.y * dt;
+    z += v.z * dt;
+    if (h != null) {
+      clearTimeout(h);
+    }
+    return h = setTimeout(report);
   };
   window.addEventListener('devicemotion', onMotion, false);
   window.alert('Starting');
