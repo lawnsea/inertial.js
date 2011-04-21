942bfbed873eebf741d0b9116327febc054fe0ba
Lon Ingram <lingram@rgmadvisors.com>
2011-04-21 11:26:53 -0500

    Push compiled source

--- inertial.js
+++ inertial.js
@@ -1,5 +1,5 @@
 (function() {
-  var INCHES_PER_METER, MOTION_REPORT_INTERVAL, body, h, motions, onMotion, positionTemplate, print, printTemplate, r, report, reportTimeout, tLastMotionEvent, v, x, y, z;
+  var INCHES_PER_METER, MOTION_REPORT_INTERVAL, axEl, ayEl, azEl, body, h, log, motions, onMotion, positionTemplate, print, printTemplate, r, report, reportTimeout, tLastMotionEvent, v, vxEl, vyEl, vzEl, x, xEl, y, yEl, z, zEl;
   x = 0;
   y = 0;
   z = 0;
@@ -16,8 +16,9 @@
   tLastMotionEvent = null;
   body = $('body');
   printTemplate = _.template('<div><span><%= s %></span></div>');
+  log = $('#log');
   print = function(s) {
-    return body.prepend(printTemplate({
+    return log.prepend(printTemplate({
       s: s
     }));
   };
@@ -34,16 +35,34 @@
     report();
     return h = null;
   };
+  axEl = $('#ax');
+  ayEl = $('#ay');
+  azEl = $('#az');
+  vxEl = $('#vx');
+  vyEl = $('#vy');
+  vzEl = $('#vz');
+  xEl = $('#x');
+  yEl = $('#y');
+  zEl = $('#z');
   onMotion = function(e) {
     var a, dt;
     a = e.acceleration;
     dt = e.interval;
-    v.x += a.x * dt;
-    v.y += a.y * dt;
-    v.z += a.z * dt;
+    v.x += (a.x * dt) / 1000;
+    v.y += (a.y * dt) / 1000;
+    v.z += (a.z * dt) / 1000;
     x += v.x * dt;
     y += v.y * dt;
     z += v.z * dt;
+    axEl.html(a.x);
+    ayEl.html(a.y);
+    azEl.html(a.z);
+    vxEl.html(v.x);
+    vyEl.html(v.y);
+    vzEl.html(v.z);
+    xEl.html(x);
+    yEl.html(y);
+    zEl.html(z);
     if (!(h != null)) {
       return h = setTimeout(reportTimeout, 500);
     }
